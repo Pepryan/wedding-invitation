cb31185da705839e5509959742e943dcb1e3a8c6
Febryan Ramadhan <febryanramadhan@gmail.com>
2024-12-03 08:25:39 +0700

    fixed bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
     // Handle service worker unregistration
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
       navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        for(let registration of registrations) {
+        for(const registration of registrations) {
           registration.unregister();
         }
       });
